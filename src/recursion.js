77812e25b2dad5ee211363f7f873880798f08cdb
sadie <thesadie@gmail.com>
2018-08-23 19:51:36 -0700

    Implements arraySum and sumBelow

--- src/recursion.js
+++ src/recursion.js
@@ -36,6 +36,15 @@ var sum = function(array) {
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+	
+	if (array.length === 0) {
+		return 0;
+	} else if (typeof array === 'number') {
+		return array;
+	} else {
+		return arraySum(array[0]) + arraySum(array.slice(1))
+	}
+
 };
 
 // 4. Check if a number is even.
@@ -54,6 +63,15 @@ var isEven = function(n) {
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
+	if (n === 0) {
+		return 0;
+	} else {
+		if (n < 0) {
+			return (n + 1) + sumBelow(n + 1);
+		} else {
+			return (n - 1) + sumBelow(n - 1);
+		}		
+	}
 };
 
 // 6. Get the integers within a range (x, y).
